707794111358644fd85640f2274da44e43ce6230
MrGessio <gregor5@onet.pl>
2025-01-02 20:36:23 +0100

    added helloWorld

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "hello world" << std::endl;
+    // nie robic std::cout<<"hello world"<<std::endl; (spacje) coding standard wyuczyc
+
+    //
+    return 0;
+}
