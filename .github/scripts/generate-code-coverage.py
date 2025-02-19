49a8fb81901e426a4f09f72c4469a0563a18216e
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2025-02-19 19:35:00 +0100

    fixed call subproces in python script

--- .github/scripts/generate-code-coverage.py
+++ .github/scripts/generate-code-coverage.py
@@ -6,7 +6,7 @@ import subprocess
 home_dir = os.getcwd()
 
 os.chdir('tests')
-subprocess.run(['./testUser'])
+subprocess.run(['./', 'testUser'])
 
 os.chdir(home_dir)
 subprocess.run(['lcov', '-c', '-d', '.', '--remove', 'libreoffice_total.info', 'tests/*', '-o', 'code-coverage.info', '--include', '*.cpp', '--include', '*.hpp', '--ignore-errors', 'mismatch'])
