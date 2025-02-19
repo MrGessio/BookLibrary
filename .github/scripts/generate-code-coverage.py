1382fde7bcc46f1c766e6b8d53ad9ef3288920c7
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2025-02-19 23:41:23 +0100

    debug

--- .github/scripts/generate-code-coverage.py
+++ .github/scripts/generate-code-coverage.py
@@ -10,5 +10,5 @@ subprocess.run(['ctest', '-C', 'Debug', '--verbose'])
 
 os.chdir(home_dir)
 subprocess.run(['lcov', '-c', '-d', '.', '-o', 'code-coverage.info', '--include', '*.cpp', '--include', '*.h', '--ignore-errors', 'mismatch'])
-subprocess.run(['lcov', '--remove', 'initial-coverage.info', 'tests/*', '-o', 'code-coverage.info'])
+# subprocess.run(['lcov', '--remove', 'initial-coverage.info', 'tests/*', '-o', 'code-coverage.info'])
 subprocess.run(['genhtml', 'code-coverage.info', '--output-directory', 'code_coverage'])
