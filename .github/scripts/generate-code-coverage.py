c63db92fc9362ec2166b7a1edb450ca9f8f9751f
Milosz Barylowicz <milosz.barylowicz@gmail.com>
2025-02-19 19:28:15 +0100

    introducing python script to generate code coverage

---
+++ .github/scripts/generate-code-coverage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+home_dir = os.getcwd()
+
+os.chdir('tests')
+subprocess.run(['./testUser'])
+
+os.chdir(home_dir)
+subprocess.run(['lcov', '-c', '-d', '.', '--remove', 'libreoffice_total.info', 'tests/*', '-o', 'code-coverage.info', '--include', '*.cpp', '--include', '*.hpp', '--ignore-errors', 'mismatch'])
+subprocess.run(['genhtml', 'code-coverage.info', '--output-directory', 'code_coverage'])
